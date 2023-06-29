11d418893242c38a1c5c298384c1d7465bbec221
Alexanua <86alesha@gafe.molndal.se>
2023-06-29 10:46:37 +0200

    update 1

--- src/main/java/org/campusmolndal/Main.java
+++ src/main/java/org/campusmolndal/Main.java
@@ -3,6 +3,7 @@ package org.campusmolndal;
 import java.util.Scanner;
 
 public class Main {
+    // kör
     public static void main(String[] args) {
         DatabaseHandler databaseHandler = new DatabaseHandler();
 
